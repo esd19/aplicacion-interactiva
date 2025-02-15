d26a183740b982e2dfc3cf65fb2827697916f21f
esd19 <40478879+esd19@users.noreply.github.com>
2025-02-14 20:18:58 -0600

    Add files via upload

---
+++ Assets/DragSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class DragSprite : MonoBehaviour
+{
+    private Vector3 offset;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main; // Obtener la cámara principal
+    }
+
+    void OnMouseDown()
+    {
+        offset = transform.position - GetMouseWorldPosition();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + offset;
+    }
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = cam.WorldToScreenPoint(transform.position).z; // Mantiene la profundidad
+        return cam.ScreenToWorldPoint(mousePoint);
+    }
+}
